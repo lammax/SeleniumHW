502c6445c248a5a0f1c35477b32493e8bb755e01
Maksim Lamansliy <maksim.lamanskiy@area9.dk>
2016-11-16 13:57:46 +0700

    Refresh webdriver-test folder

--- webdriver-test/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ webdriver-test/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -1,15 +1,13 @@
 package test.java.ru.stqa.training.selenium;
 
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
 
 public class MyFirstTest {
 
